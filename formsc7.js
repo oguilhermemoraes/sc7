306d9800270e35a2213c484bafa332e8c018e829
oguilhermemoraes <115674991+oguilhermemoraes@users.noreply.github.com>
2022-10-12 22:59:23 -0300

    Add files via upload

---
+++ formsc7.js
@@ -0,0 +1,223 @@
+<script type="text/javascript">
+    /** This section is only needed once per page if manually copying **/
+    if (typeof MauticSDKLoaded == 'undefined') {
+  var MauticSDKLoaded = true;
+  var head            = document.getElementsByTagName('head')[0];
+  var script          = document.createElement('script');
+  script.type         = 'text/javascript';
+  script.src          = 'https://marketing.baristawave.com/media/js/mautic-form.js?v5ad4a45a';
+  script.onload       = function() {
+  MauticSDK.onLoad();
+}
+;head.appendChild(script);
+  var MauticDomain = 'https://marketing.baristawave.com';
+  var MauticLang   = {
+  'submittingMessage':"Por favor,aguarde..."
+}
+}else if (typeof MauticSDK != 'undefined') {
+  MauticSDK.onLoad();
+}
+</script>
+
+
+<style type="text/css" scoped>
+    .mauticform_wrapper {
+  max-width:440px;
+  margin:10px auto;
+  font-family:Roboto;
+}
+#mauticform_label_inscricaosemanadacafeteria7_nome_completo {
+  visibility:hidden;
+}
+#mauticform_inscricaosemanadacafeteria7_em_que_fase_esta_seu_proj1 {
+  margin-top:20px;
+}
+#mauticform_label_inscricaosemanadacafeteria7_seu_melhor_email {
+  visibility:hidden;
+}
+#mauticform_label_inscricaosemanadacafeteria7_celular {
+  visibility:hidden;
+}
+#mauticform_input_inscricaosemanadacafeteria7_celular {
+  margin-top:-10px;
+}
+#mauticform_inscricaosemanadacafeteria7_seu_melhor_email {
+  margin-top:-10px;
+}
+input::placeholder {
+  font-style:normal;
+  font-weight:400;
+  font-size:16px;
+  color:#000000;
+  opacity:0.7;
+}
+}.mauticform-innerform {
+  }.mauticform-post-success {
+  }.mauticform-name {
+  font-weight:bold;
+  font-size:1.5em;
+  margin-bottom:3px;
+}
+.mauticform-description {
+  margin-top:2px;
+  margin-bottom:10px;
+}
+.mauticform-error {
+  margin-bottom:10px;
+  color:red;
+}
+.mauticform-message {
+  margin-bottom:10px;
+  color:green;
+}
+.mauticform-row {
+  display:block;
+  margin-bottom:px;
+}
+.mauticform-label {
+  font-size:em;
+  display:block;
+  font-weight:bold;
+  margin-bottom:px;
+}
+.mauticform-row.mauticform-required .mauticform-label:after {
+  color:#e32;
+  content:" *";
+  display:inline;
+}
+.mauticform-helpmessage {
+  display:block;
+  font-size:0.9em;
+  margin-bottom:3px;
+}
+.mauticform-errormsg {
+  display:block;
+  color:red;
+  margin-top:2px;
+}
+.mauticform-selectbox,.mauticform-input,.mauticform-textarea {
+  width:100%;
+  padding:16px 16px;
+  border:1px solid #CCC
+  box-shadow:0px 0px 0px #fff inset;
+  border-radius:8px;
+  box-sizing:border-box;
+  background:#FFFFFF;
+}
+.mauticform-checkboxgrp-row {
+  }.mauticform-checkboxgrp-label {
+  font-weight:normal;
+}
+.mauticform-checkboxgrp-checkbox {
+  }.mauticform-radiogrp-row {
+  }.mauticform-radiogrp-label {
+  font-weight:normal;
+  font-size:16px;
+  line-height:160%;
+}
+.mauticform-radiogrp-radio {
+  }.mauticform-button-wrapper .mauticform-button.btn-default,.mauticform-pagebreak-wrapper .mauticform-pagebreak.btn-default {
+  color:#000;
+  background-color:#ffd000;
+  border-color:#ffd000;
+}
+.mauticform-button-wrapper .mauticform-button,.mauticform-pagebreak-wrapper .mauticform-pagebreak {
+  display:inline-block;
+  margin-top:16px;
+  font-weight:600;
+  text-align:center;
+  vertical-align:center;
+  cursor:pointer;
+  background-color:#ffd000;
+  border:1px solid transparent;
+  padding:6px 12px;
+  font-size:16px;
+  line-height:1.3856;
+  border-radius:8px;
+  width:100%;
+  height:48px;
+  -webkit-user-select:none;
+  -moz-user-select:none;
+  -ms-user-select:none;
+  user-select:none;
+}
+.mauticform-button-wrapper .mauticform-button.btn-default[disabled],.mauticform-pagebreak-wrapper .mauticform-pagebreak.btn-default[disabled] {
+  background-color:#ffffff;
+  border-color:#dddddd;
+  opacity:0.75;
+  cursor:not-allowed;
+}
+.mauticform-pagebreak-wrapper .mauticform-button-wrapper {
+  display:inline;
+}
+</style><style type="text/css" scoped>
+    .mauticform-field-hidden {
+  display:none
+}
+</style>
+
+<div id="mauticform_wrapper_inscricaosemanadacafeteria7" class="mauticform_wrapper">
+    <form autocomplete="false" role="form" method="post" action="https://marketing.baristawave.com/form/submit?formId=10" id="mauticform_inscricaosemanadacafeteria7" data-mautic-form="inscricaosemanadacafeteria7" enctype="multipart/form-data">
+        <div class="mauticform-error" id="mauticform_inscricaosemanadacafeteria7_error"></div>
+        <div class="mauticform-message" id="mauticform_inscricaosemanadacafeteria7_message"></div>
+        <div class="mauticform-innerform">
+            
+          <div class="mauticform-page-wrapper mauticform-page-1" data-mautic-form-page="1">
+
+            <div id="mauticform_inscricaosemanadacafeteria7_nome_completo" data-validate="nome_completo" data-validation-type="text" class="mauticform-row mauticform-text mauticform-field-1 mauticform-required">
+                <label id="mauticform_label_inscricaosemanadacafeteria7_nome_completo" for="mauticform_input_inscricaosemanadacafeteria7_nome_completo" class="mauticform-label" >Nome completo</label>
+                <input id="mauticform_input_inscricaosemanadacafeteria7_nome_completo" name="mauticform[nome_completo]" value="" class="mauticform-input" type="text" placeholder="Nome completo *">
+                <span class="mauticform-errormsg" style="display:none;
+  ">Isso é obrigatório</span>
+            </div>
+
+            <div id="mauticform_inscricaosemanadacafeteria7_seu_melhor_email" data-validate="seu_melhor_email" data-validation-type="email" class="mauticform-row mauticform-email mauticform-field-2 mauticform-required">
+                <label id="mauticform_label_inscricaosemanadacafeteria7_seu_melhor_email" for="mauticform_input_inscricaosemanadacafeteria7_seu_melhor_email" class="mauticform-label">Seu melhor e-mail</label>
+                <input id="mauticform_input_inscricaosemanadacafeteria7_seu_melhor_email" name="mauticform[seu_melhor_email]" value="" class="mauticform-input" type="email" placeholder="Seu melhor e-mail *">
+                <span class="mauticform-errormsg" style="display:none;
+  ">Isso é obrigatório</span>
+            </div>
+
+            <div id="mauticform_inscricaosemanadacafeteria7_celular" data-validate="celular" data-validation-type="tel" class="mauticform-row mauticform-tel mauticform-field-3 mauticform-required">
+                <label id="mauticform_label_inscricaosemanadacafeteria7_celular" for="mauticform_input_inscricaosemanadacafeteria7_celular" class="mauticform-label" >Celular</label>
+                <input id="mauticform_input_inscricaosemanadacafeteria7_celular" name="mauticform[celular]" value="" class="mauticform-input" type="tel" placeholder="Celular *">
+                <span class="mauticform-errormsg" style="display:none;
+  ">Isso é obrigatório</span>
+            </div>
+
+            <div id="mauticform_inscricaosemanadacafeteria7_em_que_fase_esta_seu_proj1" data-validate="em_que_fase_esta_seu_proj1" data-validation-type="radiogrp" class="mauticform-row mauticform-radiogrp mauticform-field-4 mauticform-required">
+                <label class="mauticform-label" for="mauticform_radiogrp_radio_em_que_fase_esta_seu_proj1_1Aindaestouimaginandomeuprojeto1">Em que fase está seu projeto de cafeteria?</label>
+                <div class="mauticform-radiogrp-row">                    <input name="mauticform[em_que_fase_esta_seu_proj1]" class="mauticform-radiogrp-radio" id="mauticform_radiogrp_radio_em_que_fase_esta_seu_proj1_00" type="radio" value="0">
+                    <label id="mauticform_radiogrp_label_em_que_fase_esta_seu_proj1_00" for="mauticform_radiogrp_radio_em_que_fase_esta_seu_proj1_00" class="mauticform-radiogrp-label">1. Ainda estou imaginando meu projeto</label>
+                </div>
+                <div class="mauticform-radiogrp-row">                    <input name="mauticform[em_que_fase_esta_seu_proj1]" class="mauticform-radiogrp-radio" id="mauticform_radiogrp_radio_em_que_fase_esta_seu_proj1_11" type="radio" value="1">
+                    <label id="mauticform_radiogrp_label_em_que_fase_esta_seu_proj1_11" for="mauticform_radiogrp_radio_em_que_fase_esta_seu_proj1_11" class="mauticform-radiogrp-label">2. Já estou começando com alguns planejamentos</label>
+                </div>
+                <div class="mauticform-radiogrp-row">                    <input name="mauticform[em_que_fase_esta_seu_proj1]" class="mauticform-radiogrp-radio" id="mauticform_radiogrp_radio_em_que_fase_esta_seu_proj1_22" type="radio" value="2">
+                    <label id="mauticform_radiogrp_label_em_que_fase_esta_seu_proj1_22" for="mauticform_radiogrp_radio_em_que_fase_esta_seu_proj1_22" class="mauticform-radiogrp-label">3. Já estou mais avançado nos meus planos</label>
+                </div>
+                <div class="mauticform-radiogrp-row">                    <input name="mauticform[em_que_fase_esta_seu_proj1]" class="mauticform-radiogrp-radio" id="mauticform_radiogrp_radio_em_que_fase_esta_seu_proj1_33" type="radio" value="3">
+                    <label id="mauticform_radiogrp_label_em_que_fase_esta_seu_proj1_33" for="mauticform_radiogrp_radio_em_que_fase_esta_seu_proj1_33" class="mauticform-radiogrp-label">4. Já comecei a executar o projeto</label>
+                </div>
+                <div class="mauticform-radiogrp-row">                    <input name="mauticform[em_que_fase_esta_seu_proj1]" class="mauticform-radiogrp-radio" id="mauticform_radiogrp_radio_em_que_fase_esta_seu_proj1_44" type="radio" value="4">
+                    <label id="mauticform_radiogrp_label_em_que_fase_esta_seu_proj1_44" for="mauticform_radiogrp_radio_em_que_fase_esta_seu_proj1_44" class="mauticform-radiogrp-label">5. Estou em fase de execução final</label>
+                </div>
+                <div class="mauticform-radiogrp-row">                    <input name="mauticform[em_que_fase_esta_seu_proj1]" class="mauticform-radiogrp-radio" id="mauticform_radiogrp_radio_em_que_fase_esta_seu_proj1_55" type="radio" value="5">
+                    <label id="mauticform_radiogrp_label_em_que_fase_esta_seu_proj1_55" for="mauticform_radiogrp_radio_em_que_fase_esta_seu_proj1_55" class="mauticform-radiogrp-label">6. Já tenho uma cafeteria e estou operando</label>
+                </div>
+                <span class="mauticform-errormsg" style="display:none;
+  ">Isso é obrigatório</span>
+            </div>
+
+            <div id="mauticform_inscricaosemanadacafeteria7_submit" class="mauticform-row mauticform-button-wrapper mauticform-field-5">
+                <button type="submit" name="mauticform[submit]" id="mauticform_input_inscricaosemanadacafeteria7_submit" value="" class="mauticform-button btn btn-default">Enviar</button>
+            </div>
+            </div>
+        </div>
+
+        <input type="hidden" name="mauticform[formId]" id="mauticform_inscricaosemanadacafeteria7_id" value="10">
+        <input type="hidden" name="mauticform[return]" id="mauticform_inscricaosemanadacafeteria7_return" value="">
+        <input type="hidden" name="mauticform[formName]" id="mauticform_inscricaosemanadacafeteria7_name" value="inscricaosemanadacafeteria7">
+
+        </form>
+</div>
